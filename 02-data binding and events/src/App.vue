8a84501b0355fedbdc77bf0348c1b680449d7fda
Sarath Bodepudi <sarath.bodepudi@mutualofomaha.com>
2021-06-22 07:40:17 -0500

    data binding and events

---
+++ 02-data binding and events/src/App.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Registration from './components/Registration.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Registration
+  }
+});
+</script>
+
+
+<template>
+  <div class="content-container">
+    <div>
+        <h2 class="title"> User Registration</h2>
+    </div>
+  <div class="column">
+    <registration />
+  </div>
+  </div>
+  
+</template>
+
+<!--
+<style>
+  @import  '/design/index.scss';
+</style>
+-->
+
+<style scoped lang="css">
+@import "/design/index.scss";
+</style>
---
+++ 02-data binding and events/src/components/Registration.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div>
+      <h1 class="title">User Information</h1>
+    </div>
+
+    <div class="color-line">
+      <div class="section columns">
+        <label for="firstName">first name</label>
+        <input id="firstName" />
+      </div>
+
+      <div class="field">
+        <label for="lastName">last name</label>
+        <input id="lastName" />
+      </div>
+      
+    </div>
+
+    <div>
+      <button>
+        <i></i>
+        <span>Cancel</span>
+      </button>
+      <button>
+        <i></i>
+        <span>Save</span>
+      </button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Registration",
+};
+</script>
+
+
+
+<!--<style scoped src="../design/index.scss"></style> -->
+
+
+<style scoped lang="css">
+@import "/design/index.scss";
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
